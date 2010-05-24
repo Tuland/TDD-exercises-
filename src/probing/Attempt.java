38c2fe38f645f96e7c649edef1df7076afa10093
Tullio <tuland@gmail.com>
2010-05-24 18:15:13 +0200

    commit-ok

--- src/probing/Attempt.java
+++ src/probing/Attempt.java
@@ -7,13 +7,11 @@ public class Attempt {
 	public int add(int a, int b) { return a+b; }
 	
 	public List<String> concatenate(List<String> l, List<String> a, List<String> b) {
-		int ind = 0; 
 		Iterator<String> it_a = a.iterator();
 		Iterator<String> it_b = b.iterator();
 		while (it_a.hasNext() && it_b.hasNext()) {
 			l.add(it_a.next().concat(it_b.next()));
 		}
-		//ind++;
 		while (it_a.hasNext()){
 			l.add(it_a.next());
 		}
